8cffe07dd9db1609d99dda6773296fb26ea70570
むずでょ <muzudho1@gmail.com>
2019-07-15 21:42:34 +0900

    write

--- go-convert.py
+++ go-convert.py
@@ -3,16 +3,30 @@
 #
 # Root directory: Visual studio code workspace root.
 #
-file_name = "./ascii-floor-map-to-csv/data/floor-map.txt"
+input_file_name = "./ascii-floor-map-to-csv/data/floor-map.txt"
+output_file_name = "./ascii-floor-map-to-csv/data/floor-map.csv"
 
 try:
-    file = open(file_name)
-    lines = file.readlines()
-    for line in lines:
-        for char in line:
-            print(char)
-        print("[new line]")
+    in_file = open(input_file_name)
+
+    try:
+        out_file = open(output_file_name, 'w', encoding='utf-8')
+
+        out_file.write("X,Y,CHAR\n")
+
+        lines = in_file.readlines()
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                if char != '\n':
+                    out_file.write("{},{},{}\n".format(x, y, char))
+    except Exception as e:
+        print(e)
+    finally:
+        out_file.close()
+
 except Exception as e:
     print(e)
 finally:
-    file.close()
+    in_file.close()
+
+print("Info    : Finished.")
